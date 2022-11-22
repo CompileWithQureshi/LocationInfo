5734342d23355ba318e5dc594569a12ffa9026e5
CompileWithQureshi <99005823+CompileWithQureshi@users.noreply.github.com>
2022-11-22 07:45:24 -0800

    Added NavBar

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import NavBar from './NavBar';
+import './App.css'
+
+const App = () => {
+    return (
+        <NavBar />
+    )
+}
+export default App
---
+++ src/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css'
+
+
+
+const NavBar = () => {
+    return (
+        <nav>
+            <div className='container'>
+                <img src='./Fill 213.png' alt='logo-img' className='logo-image' />
+                <h2 className='topic'>My Travel Journal</h2>
+
+            </div>
+        </nav>
+    )
+}
+export default NavBar
