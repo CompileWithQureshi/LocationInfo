d1777b3e78eacfdd6c5d988b252ed8216927cd01
CompileWithQureshi <99005823+CompileWithQureshi@users.noreply.github.com>
2022-11-23 05:22:34 -0800

    Added info in props

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ const App = () => {
     return (
         <div className='overflow'>
             <NavBar />
-            <Hero />
-            <Hero />
-            <Hero />
-            <Hero />
+            <Hero img='./Rectangle 77.png' country="JAPAN" city='MOUNT Fuji' date='12 jan, 2021-24 jan,2021' info='Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.' />
+            <Hero img='./Rectangle 78.png' country="AUSTRALIA"
+                city='Sydney Opera House'
+                date='27 May, 2021 - 8 Jun, 2021' info='The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th centurys most famous and distinctive buildings' />
+            <Hero img='./Rectangle 84.png' country="NORWAY" city='Geirangerfjord' date='01 Oct, 2021 - 18 Nov, 2021' info='The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.' />
+
         </div>
 
     )
