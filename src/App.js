e98e48bdd6a31ee330be3023f864eef11c24c31d
CompileWithQureshi <99005823+CompileWithQureshi@users.noreply.github.com>
2022-11-24 01:30:44 -0800

    added props as const

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const App = () => {
                 city='Sydney Opera House'
                 date='27 May, 2021 - 8 Jun, 2021' info='The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th centurys most famous and distinctive buildings' />
             <Hero img='./Rectangle 84.png' country="NORWAY" city='Geirangerfjord' date='01 Oct, 2021 - 18 Nov, 2021' info='The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.' />
+            <Hero img='./Rectangle 84.png' country="NORWAY" city='Geirangerfjord' date='01 Oct, 2021 - 18 Nov, 2021' info='The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.' />
 
         </div>
 
--- src/Hero.js
+++ src/Hero.js
@@ -5,21 +5,22 @@ import logo from './Fill 219.jpg'
 
 
 const Hero = (props) => {
+    const { img, country, city, date, info } = props
     return (
         <div className='container'>
             <div className='card'>
-                <img src={props.img} alt='Img-of-the-place' className='container-Image' />
+                <img src={img} alt='Img-of-the-place' className='container-Image' />
                 <div className='data-container'>
                     <span>
                         <img src={logo} alt='logo-img' className='logo' />
 
                     </span>
                     <span>
-                        <b>{props.country}</b>
+                        <b>{country}</b>
                     </span><br />
-                    <h1><b>{props.city}</b></h1><br />
-                    <span><b>{props.date}</b></span><br /><br />
-                    <p className='paragraf'>{props.info}</p><br />
+                    <h1><b>{city}</b></h1><br />
+                    <span><b>{date}</b></span><br /><br />
+                    <p className='paragraf'>{info}</p><br />
 
                 </div>
 
